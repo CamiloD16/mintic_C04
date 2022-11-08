e382f24185333c458f84ac3370427540dedbe64f
camilo <kdodino1999@gmail.com>
2022-11-07 20:18:04 -0500

    added private routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,27 @@
 import Template from './components/Template'
 import Home from './pags/Home'
 import Login from './pags/Login'
+import PrivateRoute from './utils/PrivateRoutes'
 
 import './tailwind.css'
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
     <div className="App">
-      <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Template />}>
-          <Route path="login" element={<Login />} />
-          <Route path="home" element={<Home/>} />
-        </Route>
-      </Routes>
-      </BrowserRouter>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Template />}>
+            <Route path="login" element={<Login />} />
+          </Route>
+          <Route element={<PrivateRoute/>}>
+            <Route path='/' element={<Template />}>
+              <Route path="home" element={<Home/>} />
+            </Route>
+          </Route>
+        </Routes>
+      </Router>
     </div>
   )
 }
--- client/src/pags/Login.jsx
+++ client/src/pags/Login.jsx
@@ -1,17 +1,17 @@
 const Login = () => {
   return (
     <div className="h-screen flex justify-center items-center">
-      <div className="max-w-screen-inner shadow-md h-2/3 flex flex-col justify-center gap-y-10 px-4 sm:px-16 py-24">
+      <form className="max-w-screen-inner shadow-md h-2/3 flex flex-col justify-center gap-y-10 px-4 sm:px-16 py-24">
         <div>
         <h2 className="mb-2">Usuario</h2>
         <input type="text" className="p-2 w-full" name="user" id="user" placeholder="Ingresa tu usuario"/>
         </div>
         <div>
-        <h2 className="mb-2">Constraseña</h2>
+        <h2 className="mb-2">Contraseña</h2>
         <input type="password" className="p-2 w-full" name="password" id="password" placeholder="Ingresa tu constraseña" />
         </div>
-        <button type="button" className="bg-sky-800 text-white px-16 sm:px-24 py-2">Iniciar Sesión</button>
-      </div>
+        <button type="submit" className="bg-sky-800 text-white px-16 sm:px-24 py-2">Iniciar Sesión</button>
+      </form>
     </div>
   )
 }
